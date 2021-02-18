0d9de214bf77880478f7f3175e0ba4a0d81297c4
Nikita Y <fmajesty@pm.me>
2021-02-18 23:29:02 +0500

    unique_id generation

--- fastapi_users/db/sqlalchemy.py
+++ fastapi_users/db/sqlalchemy.py
@@ -146,6 +146,7 @@ class SQLAlchemyUserDatabase(BaseUserDatabase[UD]):
 
     async def create(self, user: UD) -> UD:
         user_dict = user.dict(exclude={'id'})
+        user_dict["unique_id"] = uuid.uuid4()
         oauth_accounts_values = None
 
         if "oauth_accounts" in user_dict:
